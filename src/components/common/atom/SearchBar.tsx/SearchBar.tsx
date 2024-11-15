0509125fc3876769cb67159d6ffe055cf21ee5f4
Calvin Lua <86276701+calvinlua@users.noreply.github.com>
2024-11-15 17:56:48 +0800

    Update css changes on search box and search button

--- src/components/common/atom/SearchBar.tsx/SearchBar.tsx
+++ src/components/common/atom/SearchBar.tsx/SearchBar.tsx
@@ -1,8 +1,7 @@
 import { SearchRounded } from "@mui/icons-material";
-import { Autocomplete, Button, Stack, TextField } from "@mui/material";
+import { Autocomplete, Button, TextField } from "@mui/material";
 import { FormEvent } from "react";
 import { countries } from "../../../../constants/countries";
-
 import classes from "./SearchBar.module.css";
 
 const SearchBar = (props: {
@@ -10,49 +9,38 @@ const SearchBar = (props: {
   handleSearch: (e: FormEvent<HTMLFormElement>) => void;
 }) => {
   return (
-    <>
-      <Stack direction={"row"} gap={3}>
-        <form onSubmit={props.handleSearch}>
-          <>
-            <Autocomplete
-              className={classes["input-box"]}
-              freeSolo
-              options={countries}
-              getOptionLabel={(option) => option}
-              onChange={(event: any, value: any) => {
-                event.preventDefault();
-                props.setSearchCountry(value);
-              }}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  id="filled-basic"
-                  label="Country"
-                  variant="filled"
-                />
-              )}
-            />
-          </>
-          <>
-            <Button
-              className={classes["search-button"]}
-              sx={{
-                color: "primary.main",
-                backgroundColor: "primary.light",
-                borderRadius: "20px",
-              }}
-              type="submit"
-            >
-              <SearchRounded
-                sx={{
-                  color: "white",
-                }}
+    <div>
+      <form onSubmit={props.handleSearch} style={{ display: "flex " }}>
+        <>
+          <Autocomplete
+            className={classes["input-box"]}
+            freeSolo
+            options={countries}
+            getOptionLabel={(option) => option}
+            onChange={(event: any, value: any) => {
+              event.preventDefault();
+              props.setSearchCountry(value);
+            }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                variant="filled"
+                focused
+                InputProps={{ disableUnderline: true }}
+                id="filled-basic"
+                label="Country"
+                className={classes["search-field"]}
               />
-            </Button>
-          </>
-        </form>
-      </Stack>
-    </>
+            )}
+          />
+        </>
+        <>
+          <Button className={classes["search-button"]} type="submit">
+            <SearchRounded sx={{ color: "white" }} />
+          </Button>
+        </>
+      </form>
+    </div>
   );
 };
 
